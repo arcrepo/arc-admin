4428b0f4f23b3c2eb3e3f7c9c312fee7e649c281
xencodes <xencodes@icloud.com>
2020-06-23 13:22:02 +0530

    added redux support

--- src/index.js
+++ src/index.js
@@ -4,9 +4,20 @@ import "./App.less";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+import { createStore } from "redux";
+import RootReducer from "./redux/reducers";
+import { Provider } from "react-redux";
+
+const store = createStore(
+  RootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
